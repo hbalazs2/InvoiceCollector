0f819b3415be410f37f7daac1ad7173cea96d86d
Balázs Hermann <Balazs.Hermann@awacademy.training>
2022-03-29 10:50:05 +0200

    first commit

---
+++ src/main/java/com/example/InvoiceCollector/InvoiceCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.InvoiceCollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceCollectorApplication.class, args);
+	}
+
+}
