b1f3a30ca7f5a8120f586636efafb2a776c9bddb
Balázs Hermann <Balazs.Hermann@awacademy.training>
2022-03-29 11:29:47 +0200

    added DBConnector

---
+++ src/main/java/com/example/InvoiceCollector/db/DBConnector.java
@@ -0,0 +1,35 @@
+package com.example.InvoiceCollector.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+    private static DBConnector instance;
+    private Connection connection;
+
+    public static synchronized DBConnector getInstance() {
+        if (instance == null) {
+            instance = new DBConnector();
+            return instance;
+        }
+        return instance;
+    }
+
+    private DBConnector() {
+        try {
+            this.connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/weather",
+                    "admin",
+                    "Password"
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+}
