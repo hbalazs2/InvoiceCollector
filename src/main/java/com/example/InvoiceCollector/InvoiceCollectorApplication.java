12c563a96cabc391e70954aa44595b278c4cc094
Balázs Hermann <Balazs.Hermann@awacademy.training>
2022-03-30 11:03:22 +0200

    InvoiceDB select all, select by id, insert methods added and tested OK!

--- src/main/java/com/example/InvoiceCollector/InvoiceCollectorApplication.java
+++ src/main/java/com/example/InvoiceCollector/InvoiceCollectorApplication.java
@@ -1,10 +1,14 @@
 package com.example.InvoiceCollector;
 
 import db.DBConnector;
+import db.InvoiceDB;
+import model.Invoice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.time.LocalDate;
 
 @SpringBootApplication(scanBasePackages = {"controller"})
 public class InvoiceCollectorApplication {
@@ -15,9 +19,13 @@ public class InvoiceCollectorApplication {
 	}
 
 	public static void testDB() {
-		Connection connection = DBConnector.getInstance().getConnection();
-		System.out.println(connection);
-		System.out.println("OK");
+//		Connection connection = DBConnector.getInstance().getConnection();
+		InvoiceDB invoiceDB = new InvoiceDB();
+		Date complDate = Date.valueOf(LocalDate.parse("2022-03-31"));
+		Date deadline = Date.valueOf(LocalDate.parse("2022-04-01"));
+		Invoice invoice = invoiceDB.insertInvoice(new Invoice(complDate, deadline, 300000, 1));
+//		System.out.println(invoice);
+
 	}
 
 }
